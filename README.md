29089191ef27d44046c0d027ee791e0ab736f7b4
Guilherme Andrade <andrade@rarolabs.com.br>
2019-08-15 14:15:25 -0300

    recriando interface do instagram

---
+++ README.md
@@ -0,0 +1,11 @@
+# UI Clone: Instagram
+
+Interface do **Instagram** feita com React Native
+
+## Features
+
+- Scroll infinito
+- Carregar imagens no scroll do app
+- Pré-carregar imagens de forma ofuscada (*blurred*)
+- Refresh da lista de posts
+- Animação
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Feed from './pages/Feed';
+
+import logo from './assets/instagram.png';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Feed
+  }, {
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerTitle: <Image source={logo} />,
+      headerStyle: {
+        backgroundColor: '#f5f5f5'
+      }
+    }
+  })
+)
+
+export default Routes;
